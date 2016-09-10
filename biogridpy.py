9ff8a1adea50a410d3dc685b2a2cd46d0e967ddd
arvkevi <arvkevi@gmail.com>
2016-09-10 16:49:44 -0400

    Implement ResponseHandler Class

--- biogridpy.py
+++ biogridpy.py
@@ -1,6 +1,6 @@
-# biogridpy
-
 from __future__ import print_function
+from biogrid_results import ResponseHandler as BGRH
+
 
 try:
     import urlparse, types
@@ -16,7 +16,7 @@ except:
     from configparser import ConfigParser
     config = ConfigParser()
     config.read_file(open('biogridpy.ini'))
-import json
+
 
 class BioGrid(object):
     """BioGrid API"""
@@ -33,7 +33,7 @@ class BioGrid(object):
         self.ORGANISMS_URL = BASE_URL + 'organisms/'
         self.VERSION_URL = BASE_URL + 'version/'
 
-    def evidence(self, Format='tab2', tab2_format='str'):
+    def evidence(self, Format='tab2'):
         """
         Returns valid identifier types
         
@@ -73,25 +73,23 @@ class BioGrid(object):
             ids_json = self.byteify(json.loads(results))
             return ids_json
         elif Format == 'tab2':
-            if tab2_format == 'str': #return raw string w/ newline chars
-                return results
-            elif tab2_format == 'list':
-                return results.split('\n')
+            return results
+
         else:
             print ("Specify either <'json'> or <'tab2'> as arguments for the Format parameter")
             return
         
-    def identifiers(self, Format='tab2', tab2_format='str'):
+    def identifiers(self, Format='tab2', **kwargs):
         """
         Returns valid identifier types
         
         Usage:
         # Default params return a string identical to the default webservice:
         # http://webservice.thebiogrid.org/identifiers/?accesskey=[ACCESSKEY]
-        >>> id_types = BioGrid().identifiers(Format='tab2', tab2_format='str')
+        >>> id_types = BioGrid().identifiers(Format='tab2')
         
         # For a Python list of the identifiers, change tab2_format to 'list':
-        >>> id_types = BioGrid().identifiers(Format='tab2', tab2_format='list')
+        >>> id_types = BioGrid().identifiers(Format='tab2')
         
         # Finally, to replicate the "format=json" parameter:
         >>> id_types = BioGrid().identifiers(Format='json')
@@ -121,13 +119,67 @@ class BioGrid(object):
             ids_json = self.byteify(json.loads(results))
             return ids_json
         elif Format == 'tab2':
-            if tab2_format == 'str': #return raw string w/ newline chars
-                return results
-            elif tab2_format == 'list':
-                return results.split('\n')
+            return results
         else:
             print ("Specify either <'json'> or <'tab2'> as arguments for the Format parameter")
             return
+    
+    def validate_interaction_param(self, **kwargs):
+            valid_params = (
+            ('accessKey', self.access_key),
+            ('Format', kwargs.pop('Format', 'tab2')),
+            ('tab2_format', kwargs.pop('tab2_format', 'list')),
+            ('start', kwargs.pop('start', 0)),
+            ('max', kwargs.pop('max', 10000)),
+            ('interSpeciesExcluded', kwargs.pop('interSpeciesExcluded', 'false')),
+            ('selfInteractionsExcluded', kwargs.pop('selfInteractionsExcluded', 'false')),
+            ('evidenceList', kwargs.pop('evidenceList', '')), #str-like pipe-separated list of evidence codes
+            ('includeEvidence', kwargs.pop('includeEvidence', 'false')),
+            ('geneList', kwargs.pop('geneList', '')), #str-like pipe-separated list of gene names or ids
+            ('searchIds', kwargs.pop('searchIds', 'false')), #If 'true', try to match
+            ('searchNames', kwargs.pop('searchNames', 'false')), #If 'true' try to match names
+            ('searchSynonyms', kwargs.pop('searchSynonyms', 'false')),
+            ('searchBiogridIds', kwargs.pop('searchBiogridIds', 'false')),
+            ('additionalIdentifierTypes', kwargs.pop('additionalIdentifierTypes', '')), #str-like pipe-separated
+            ('excludeGenes', kwargs.pop('excludeGenes', 'false')),
+            ('includeInteractors', kwargs.pop('includeInteractors', 'true')),
+            ('includeInteractorInteractions', kwargs.pop('includeInteractorInteractions', 'false')),
+            ('pubmedList', kwargs.pop('pubmedList', '""')), #pipe-separated list of pmids
+            ('excludePubmeds', kwargs.pop('excludePubmeds', 'false')),
+            ('htpThreshold', kwargs.pop('htpThreshold', 2147483647)),
+            ('throughputTag', kwargs.pop('throughputTag', 'any')), # "any", "low", "high"
+            ('taxId', kwargs.pop('taxId', 'All')),
+            ('includeHeader', kwargs.pop('includeHeader', 'false')),
+            ('translate', kwargs.pop('translate', 'false')),
+                       )
+            
+    def interactions(self, **kwargs):
+        """
+        Format can be 'tab2', '
+        """
+        Format = kwargs.pop('Format', 'tab2')
+        
+        query_params_default = (('accessKey', self.access_key),
+                                ('Format', Format),
+                                )
+        
+        query_params_kwargs = tuple({(k,v) for k,v in kwargs.items()})
+        
+        query_params = query_params_default + query_params_kwargs
+        
+        query_params = [(param[0], param[1].encode('utf-8')
+                         if type(param[1]) is types.UnicodeType
+                         else param[1]) for param in query_params]
+                             
+        query_string = urlencode(query_params)
+        query_string = '{0}?{1}'.format(self.INTERACTIONS_URL, query_string)
+        print (query_string)
+        request = urlopen(query_string)
+        results = request.read()
+        
+        return BGRH(results, Format)
+        
+
         
     def organisms(self, Format='tab2', tab2_format='str'):
         """
@@ -165,44 +217,6 @@ class BioGrid(object):
                              
         query_string = urlencode(query_params)
         query_string = '{0}?{1}'.format(self.IDENTIFIERS_URL, query_string)
-        
-        request = urlopen(query_string)
-        results = request.read()
-        
-        if Format == 'json':
-            ids_json = self.byteify(json.loads(results))
-            return ids_json
-        elif Format == 'tab2':
-            if tab2_format == 'str': #return raw string w/ newline chars
-                return results
-            elif tab2_format == 'list':
-                return results.split('\n')
-        else:
-            print ("Specify either <'json'> or <'tab2'> as arguments for the Format parameter")
-            return
-        
-    def interactions(self, **kwargs):
-        
-        query_params = (
-            ('accessKey', self.access_key),
-            ('format', kwargs.pop('Format', 'str')),
-            ('start', kwargs.pop('start', 0)),
-            ('max', kwargs.pop('max', 10000)),
-            ('interSpeciesExcluded', kwargs.pop('interSpeciesExcluded', 'false')),
-            ('selfInteractionsExcluded', kwargs.pop('selfInteractionsExcluded', 'false')),
-            ('orfilter', kwargs.pop('orfilter', '')),
-            ('betafilter', kwargs.pop('betafilter', '')),
-            ('datefilter', kwargs.pop('datefilter', '')),
-            ('sort', kwargs.pop('sort', '')),
-            ('asc', kwargs.pop('asc', ''))
-                       )
-        
-        query_params = [(param[0], param[1].encode('utf-8')
-                         if type(param[1]) is types.UnicodeType
-                         else param[1]) for param in query_params]
-                             
-        query_string = urlencode(query_params)
-        query_string = '{0}?{1}'.format(self.search_url, query_string)
         
         request = urlopen(query_string)
         results = request.read()
